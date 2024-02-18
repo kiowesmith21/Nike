99a22e815a60fa9998b46a89297c220d85a9dcc1
kiowesmith21 <kiowesmith@gmail.com>
2024-02-18 14:52:15 -0500

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import CustomerReviews from './CustomerReviews'
+import Footer from './Footer'
+import Services from './Services'
+import SuperQuality from './SuperQuality'
+import Subscribe from './Subscribe'
+import SpecialOffer from './SpecialOffer'
+
+export {
+    Hero,
+    PopularProducts,
+    CustomerReviews,
+    Footer,
+    Services,
+    SuperQuality,
+    Subscribe,
+    SpecialOffer
+}
